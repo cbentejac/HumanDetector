1334efffc99c01f352da1a2e096f7210d53829c5
xArchange <candice.bentejac@gmail.com>
2017-02-26 17:06:21 +0100

    Modified function prototypes and did some cleaning.
    Prototypes of Extract_Templates and Process_Templates have been modified
    (+ Process_Templates has been transformed from script to function) and
    all the changes induced by these modifications have been made (+ some
    petty cleaning).

--- Extract_Templates.m
+++ Extract_Templates.m
@@ -1,26 +1,23 @@
-% close all
-% clear all
-% clc
-function Extract_Templates(video_dir, videos)
-    % Selects the folders of the input videos and creates "Templates_train".  
-%     video_dir = './Videos';
-%     videos = {'Sequence1','Sequence2','Sequence3','Sequence4', 'Sequence5', 'Sequence6', 'Sequence7'};
+function templates_directory = Extract_Templates(video_dir, videos)
+    % Selects the folders of the input videos & creates "Templates_train".  
     templates_directory = '.\Templates_train';
     mkdir Templates_train;
 
-    % Initializes the number of templates (will be used to name the templates
-    % that are going to be written out).
+    % Initializes the counter of templates (will be used to name the 
+    % templates that are going to be written out).
     num_templates = 0;
     for i = 1 : size(videos,2)    
         dir = sprintf('cd ''%s''',pwd); % Gets our current path (+ cd).
-        directory_masks = sprintf('cd %s/%s/masks/',video_dir,videos{i}); % Gets the masks' paths (+ cd).
+        directory_masks = sprintf('cd %s/%s/masks/', video_dir, ...
+            videos{i}); % Gets the masks' paths (+ cd).
         eval(directory_masks);
         number_masks = ls;    
         eval (dir) 
 
-        for j = 3 : size(number_masks,1)        
+        for j = 3 : size(number_masks, 1)        
             % Gets the name of the mask to read and loads/reads it.
-            name_mask = sprintf('%s/%s/masks/frame%.4d.png',video_dir,videos{i},j-3);
+            name_mask = sprintf('%s/%s/masks/frame%.4d.png', ...
+                video_dir, videos{i}, j - 3);
             mask = imread(name_mask);
 
             %%%Object extraction code
@@ -38,61 +35,53 @@ function Extract_Templates(video_dir, videos)
                     height = stats(k).BoundingBox(3);
                     width = stats(k).BoundingBox(4);
 
-                    % Works for those videos because there's always a body shape 
-                    % but might not work for others. Quite fast but definitely
-                    % incomplete, will be optimized later.
-                    % Basically, looks for the biggest component, assumes it's a 
-                    % body (but could literally be anything, as long as it's the 
-                    % biggest) and crops around it.
-                    if area(k).Area > max_size && (width >= 30 && height >= 30)
+                    % Basically, looks for the biggest component, assumes 
+                    % it's a body if it's bigger than 30 * 30 pixels (but 
+                    % could literally be anything, as long as it's big 
+                    % enough) and crops around it.
+                    if area(k).Area > max_size && ...
+                            (width >= 30 && height >= 30)
                         max_size = area(k).Area;
                         max_k = k;
                     end
                 end
                
                 % If one blob was deemed worthy of becoming a template,
-                % crops the image around it and saves it.
+                % crops the image around it and saves it. Else, do nothing
+                % (and thus don't try to extract a template at all).
                 if max_k > -1
                     x1 = floor(stats(max_k).BoundingBox(2));
                     y1 = floor(stats(max_k).BoundingBox(1));
                     x2 = round(x1 + stats(max_k).BoundingBox(4));
                     y2 = round(y1 + stats(max_k).BoundingBox(3));
 
-                    % Handles the cases where the object extraction code would set 
-                    % out-of-image boundaries.         
-                    if x1<=0
-                        x1=1;
+                    % Handles the cases where the object extraction code 
+                    % would set out-of-image boundaries.         
+                    if x1 <= 0
+                        x1 = 1;
                     end
-                    if y1<=0
-                        y1=1;
+                    if y1 <= 0
+                        y1 = 1;
                     end
-                    if x2>size(mask,1)
-                        x2=size(mask,1);
+                    if x2 > size(mask, 1)
+                        x2 = size(mask, 1);
                     end
-                    if y2>size(mask,2)
-                        y2=size(mask,2);
+                    if y2 > size(mask, 2)
+                        y2 = size(mask, 2);
                     end
-                    crop_image = mask(x1:x2,y1:y2); % Crops the image, displays it.
-                    % Saves/writes the template in the directory created for it.
-                    num_templates=num_templates+1; % Increases the template counter.
-                    template_name=sprintf('%s/template%.4d.png',templates_directory,num_templates);
-                    imwrite(crop_image,template_name) % Writes the template out.
-%                     pause(0.001);
+                    crop_image = mask(x1 : x2, y1 : y2); % Crops the image.
+                    
+                    % Saves the template in the directory created for it.
+                    num_templates = num_templates + 1; % Increases counter.
+                    template_name = sprintf('%s/template%.4d.png', ...
+                        templates_directory, num_templates);
+                    imwrite(crop_image, template_name) % Saves the template.
                 end
-            
             %%Code to change
             %%%Object extraction code   
-    %         figure,imshow(mask)        
-            
-    %         imshow(crop_image)
-
-%                 max_size = 0;
-%                 max_k = -1;      
             end   
-        
-        
         end
-        disp(num_templates);
-        disp([num2str(videos{i}), ' done']);
+        disp([num2str(videos{i}), ' done, ', num2str(num_templates), ...
+            ' templates extracted in total.']);
     end
 end
--- Process_Templates.m
+++ Process_Templates.m
@@ -1,67 +1,67 @@
-% close all
-% clear all
-% clc
+function Process_Templates(video_dir, templates_directory)
+    % Selects the folders of the input videos/templates and 
+    % creates "Templates_test".  
+    final_templates_directory='.\Templates_test';
+    mkdir Templates_test;
 
-% Selects the folders of the input videos/templates and creates "Templates_test".  
-video_dir='./Videos';
-templates_directory='Templates_train';
-final_templates_directory='.\Templates_test';
-mkdir Templates_test;
+    % Creates the path changes command and gets the names of the masks.
+    dir=sprintf('cd ''%s''',pwd);
+    templates_dir=sprintf('cd %s/',templates_directory);
+    eval(templates_dir);
+    number_masks=ls;
+    eval (dir);
+    ratio_values=[];
 
-% Creates the path changes command and gets the names of the masks.
-dir=sprintf('cd ''%s''',pwd);
-templates_dir=sprintf('cd %s/',templates_directory);
-eval(templates_dir);
-number_masks=ls;
-eval (dir);
-ratio_values=[];
+    % Selects the useful templates.
+     %%%Object extraction code
+     %%Code to change    
 
-% Selects the useful templates.
- %%%Object extraction code
- %%Code to change    
- 
-% Selection of the templates based on their size difference:
-% Assumption: if the size of two consecutive templates is different enough,
-% then the poses of the two silouhettes are different enough to be useful
-% and thus selected. 
-% 5 pixels has been chosen arbitrarily and seems to be producing
-% significant template extraction, but tests will need to be run to assess
-% it. (on another hand, a quick look has been given to standard deviation
-% and mean but same: tests with a "finished" detector are needed to be able
-% to really get the significant templates out in the codebook).
-chosen_templates = [];
-size_cur = [0 0];
-for j=3:size(number_masks, 1)      
-    name_mask = sprintf('%s/template%.4d.png', templates_directory, j - 2);
-    mask = imread(name_mask);
-    
-    size_tmp = size(mask);
-    if abs(size_tmp(1) - size_cur(1)) > 5 || abs(size_tmp(2) - size_cur(2)) > 5
-        chosen_templates(end + 1) = j - 2;
+    % Selection of the templates based on their size difference:
+    % Assumption: if the size of two consecutive templates is different enough,
+    % then the poses of the two silouhettes are different enough to be useful
+    % and thus selected. 
+    % 5 pixels has been chosen arbitrarily and seems to be producing
+    % significant template extraction, but tests will need to be run to assess
+    % it. (on another hand, a quick look has been given to standard deviation
+    % and mean but same: tests with a "finished" detector are needed to be able
+    % to really get the significant templates out in the codebook).
+    chosen_templates = [];
+    size_cur = [0 0];
+    for j = 3 : size(number_masks, 1)      
+        name_mask = sprintf('%s/template%.4d.png', templates_directory, ...
+            j - 2);
+        mask = imread(name_mask);
+
+        size_tmp = size(mask);
+        if abs(size_tmp(1) - size_cur(1)) > 5 || ...
+                abs(size_tmp(2) - size_cur(2)) > 5
+            chosen_templates(end + 1) = j - 2;
+        end
+        size_cur = size_tmp;
+    end    
+    disp(length(chosen_templates));
+    %     index=1:size(number_masks,1)-2;
+    %     N=100;%percentaje
+    %     step=size(index,2)/(size(index,2)*N/100);
+    %     chosen_templates=index(1:step:size(index,2));
+
+     %%%Object extraction code
+     %%Code to change  
+
+    % Reads in the selected templates, writes them into the test folder and also 
+    % writes their flip.
+    num_templates = 0;
+    for n = 1 : size(chosen_templates, 2)
+        num_templates = num_templates + 1;
+        name_mask = sprintf('%s/template%.4d.png', templates_directory, ...
+            chosen_templates(n));
+        mask = imread(name_mask);
+        template_name = sprintf('%s/template%.4d.png', ...
+            final_templates_directory, num_templates);
+        imwrite(mask, template_name)
+        num_templates = num_templates+1;
+        template_name = sprintf('%s/template%.4d.png', ...
+            final_templates_directory, num_templates);
+        imwrite(fliplr(mask), template_name)
     end
-    size_cur = size_tmp;
-end    
-disp(length(chosen_templates));
-%     index=1:size(number_masks,1)-2;
-%     N=100;%percentaje
-%     step=size(index,2)/(size(index,2)*N/100);
-%     chosen_templates=index(1:step:size(index,2));
- 
- %%%Object extraction code
- %%Code to change  
-    
-% Reads in the selected templates, writes them into the test folder and also 
-% writes their flip.
-num_templates=0;
-for n=1:size(chosen_templates,2)
-    num_templates=num_templates+1;
-    name_mask=sprintf('%s/template%.4d.png',templates_directory,chosen_templates(n));
-    mask=imread(name_mask);
-    template_name=sprintf('%s/template%.4d.png',final_templates_directory,num_templates);
-    imwrite(mask,template_name)
-    num_templates=num_templates+1;
-    template_name=sprintf('%s/template%.4d.png',final_templates_directory,num_templates);
-    imwrite(fliplr(mask),template_name)
 end
-
-    
